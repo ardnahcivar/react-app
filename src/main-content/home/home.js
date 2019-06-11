3e175a6164bb0fb5eb0a7c002f772d326c5c14bf
ravbhanage <ravbhanage@gmail.com>
2019-06-11 09:23:11 +0000

    created the background shape for home page

--- src/main-content/home/home.js
+++ src/main-content/home/home.js
@@ -4,21 +4,26 @@ import styles from  './home.module.css';
 
 
 export default class Home extends Component{
-  
+
     constructor(props){
         super(props);
     }
 
     componentDidMount(){
         console.log(this.props);
-    }   
+    }
 
     render(){
         return (
             <Aux>
-                <div className={`description ${styles.fiftyWHL}`}></div>
-                <div className={`actions ${styles.fiftyWHR} ${styles.floatRight}`}>
-                    <div className={styles.actionsContainer}>
+                <div className={styles.back}></div>
+                <div className={styles.description}>
+                    <p>Prepare  for your Verbal GRE using it Wordlist application</p>
+                    <p>Checkout the different Modes like <strong>Practice and Learn Mode</strong></p>
+                    <p>You can also the view application on <strong>Mobile with Offline Capability</strong></p>
+                </div>
+                <div className={styles.actions}>
+                    <div className={[styles.actionsContainer].join(' ')}>
                         <button tabIndex="1" className={styles.actionsBtn} onClick={this.learn}>LEARN</button>
                         <button tabIndex="2" className={styles.actionsBtn} onClick={this.practice}>PRACTICE</button>
                     </div>
